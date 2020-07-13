427232a81dad0b0fd018b4d43604e466b2d222fc
vishaldkale <vishalkale2@gmail.com>
2020-07-13 16:31:52 +0530

    First commit

---
+++ src/CHelloWorld.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace vkale\CHelloWorld;
+
+class CHelloWorld {
+
+	public function __construct() {
+		// constructor body
+	}
+
+	public function SayHello( )
+	{
+		return 'Hello This is my first package.';
+	}
+
+}
